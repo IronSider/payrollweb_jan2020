9299939c8ae9347f60fe6b34dc6fe876e4ba837e
IronSider <119492327+IronSider@users.noreply.github.com>
2021-11-13 07:08:21 +0000

    update extension

--- client/consensus/poa/benches/benchmark.rs
+++ client/consensus/poa/benches/benchmark.rs
@@ -10,4 +10,17 @@ fn generate_chunk_proof(data: Vec<u8>, offset: u32) -> ChunkProof {
         .expect("failed to build chunk proof")
 }
 
-fn random
+fn random_data(data_size: usize) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    (0..data_size).map(|_| rng.gen::<u8>()).collect()
+}
+
+fn chunk_proof_benchmark(c: &mut Criterion) {
+    let data = random_data(10 * 1024 * 1024);
+    c.bench_function("chunk proof generation 10MiB", |b| {
+        b.iter(|| generate_chunk_proof(black_box(data.clone()), black_box(20)))
+    });
+
+    let data = random_data(100 * 1024 * 1024);
+    c.bench_function("chunk proof generation 100MiB", |b| {
+        b.iter(|| generate_chunk_proof(b
