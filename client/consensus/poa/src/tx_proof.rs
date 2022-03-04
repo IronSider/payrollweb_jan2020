aa87b0c35bca4b1d60402bf5f9011dd21b99f2a7
IronSider <119492327+IronSider@users.noreply.github.com>
2022-03-04 20:31:58 +0000

    simplifications

--- client/consensus/poa/src/tx_proof.rs
+++ client/consensus/poa/src/tx_proof.rs
@@ -8,4 +8,18 @@
 // by the Free Software Foundation, either version 3 of the License,
 // or (at your option) any later version.
 //
-// Canyon
+// Canyon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Canyon. If not, see <http://www.gnu.org/licenses/>.
+
+use codec::Encode;
+
+use sp_core::H256;
+use sp_runtime::traits::Block as BlockT;
+
+use canyon_primitives::ExtrinsicIndex;
+use cp_consensus_poa::enc
