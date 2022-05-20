3e371042193d28fd5140104672839fee124e22ed
IronSider <119492327+IronSider@users.noreply.github.com>
2022-05-20 16:21:53 +0000

    Fix some memory leaks.

--- pallets/poa/src/benchmarking.rs
+++ pallets/poa/src/benchmarking.rs
@@ -17,4 +17,20 @@
 // along with Canyon. If not, see <http://www.gnu.org/licenses/>.
 
 use super::*;
-use cp_consensus_poa::{ChunkProof, PoaConfigu
+use cp_consensus_poa::{ChunkProof, PoaConfiguration, ProofOfAccess};
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite};
+use frame_system::RawOrigin;
+use sp_std::vec;
+
+pub(crate) fn mock_a_data_chunk() -> Vec<u8> {
+    const ALPHABET: [u8; 32] = [
+        b'q', b'w', b'e', b'r', b't', b'y', b'u', b'i', b'o', b'p', b'[', b']', b'a', b's', b'd',
+        b'f', b'g', b'h', b'j', b'k', b'l', b';', b'z', b'x', b'c', b'v', b'b', b'n', b'm', b',',
+        b'.', b' ',
+    ];
+
+    ALPHABET.repeat(8192)
+}
+
+benchmarks! {
+    // This will measure the execution time of `deposit
