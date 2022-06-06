3d3e12b60021a209d105858167f7695b05f44e2e
IronSider <119492327+IronSider@users.noreply.github.com>
2022-06-06 12:06:18 +0000

    Update patterns.md

--- pallets/poa/src/mock.rs
+++ pallets/poa/src/mock.rs
@@ -9,4 +9,18 @@
 // or (at your option) any later version.
 //
 // Canyon is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the impli
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Canyon. If not, see <http://www.gnu.org/licenses/>.
+
+use super::*;
+
+use frame_support::{parameter_types, traits::Everything};
+use sp_core::H256;
+// The testing primitives are very useful for avoiding having to work with signatures
+// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
+use sp_runtime::{
+ 
