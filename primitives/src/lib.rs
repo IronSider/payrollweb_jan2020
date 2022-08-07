c8b7c69e9a8f993e3e7bca41f9888c70d36bd5e6
IronSider <119492327+IronSider@users.noreply.github.com>
2022-08-07 22:54:21 +0000

    Corrected version.

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -18,4 +18,20 @@
 
 //! Low-level types used throughout the Canyon code.
 
-#![warn(missing_do
+#![warn(missing_docs)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use sp_runtime::{
+    generic,
+    traits::{BlakeTwo256, IdentifyAccount, Verify},
+    MultiSignature, OpaqueExtrinsic,
+};
+
+/// An index to a block.
+pub type BlockNumber = u32;
+
+/// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
+pub type Signature = MultiSignature;
+
+/// Some way of identifying an account on the chain. We intentionally make it equivalent
+/// to the public key of our transaction signing scheme.
